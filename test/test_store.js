e98620eae7c59f63e3ebbca1e2351fcafb531e6c
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-04 11:43:12 -0700

    Save test working(passed)

--- test/test_store.js
+++ test/test_store.js
@@ -4,18 +4,24 @@ const Store = require('../lib/store.js');
 
 describe('in memory database', () =>{
 
+    let id; //declare a variable id to be called in other test functions.
+
     const movies = new Store();
 
     it('save', () => {
         let newMovie = { title: 'Moulin Rouge (2001)', director: 'Baz Luhrmann', duration: '2h 7min', genre: 'Drama, Musical, Romance', awards: '2 Oscars' }
       
-    
-        movies.save(newMovie);
-        console.log(newMovie);
+        let result = movies.save(newMovie); //result now equals the object "item" generated 
+                                            //from the save function is store.js
+
+        id = result._id; //set your outer scoped id variable to the _.id of the result object, so you can use it later
+        assert.ok(result._id);
+        console.log(result);
 
     })
 
     it('get', () => {
+
 
 
     })
