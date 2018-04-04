f858b4a595418aadc7155f384287701501d659ab
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-04 10:35:47 -0700

    setting up tests templates for test_store.js

--- test/test_store.js
+++ test/test_store.js
@@ -2,11 +2,29 @@ const assert = require('assert');
 const store = require('../lib/store.js');
 const shortid = require('shortid');
 
-describe('return movie title and id from array', () =>{
+describe('in memory database', () =>{
+
+    it('save', () => {
+
+
+    })
+
+    it('get', () => {
+
+
+    })
+
+    it('getAll', () =>{
+
+
+    })
+
+    it('remove', () =>{
+
+
+    })
 
 
 
 
-
-
-})
+});
